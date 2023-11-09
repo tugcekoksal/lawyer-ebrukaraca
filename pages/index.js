76e23f1b6c6356052107de014f83416cf53f4cd3
tugcekoksal <tugcekoksal35@gmail.com>
2023-11-09 14:41:26 +0100

    change

--- pages/index.js
+++ pages/index.js
@@ -8,24 +8,19 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Avukat Ebru KARACA
-          </a>
-        </div>
+      <div className="flex justify-center items-center">
+        <p className=" ">Avukat Ebru KARACA</p>
       </div>
 
-      <div className="relative mb-[30%] z-[9999]  flex w-[60%] place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full  before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
+      <div className="relative mt-[10px] z-[9999]  flex w-[60%] place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full  before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
         <h1 className="text-3xl leading-loose ">
           Sitemiz yapım aşamasındadır. Tüm sorularınız için{" "}
           <span className=" underline ">
-            <a href={"mailto:avebrukoksal@gmail.com"} className="text-red-400">
+            <a
+              href={"mailto:avebrukoksal@gmail.com"}
+              target="_blank"
+              className="text-red-400"
+            >
               avebrukoksal@gmail
             </a>
           </span>{" "}
