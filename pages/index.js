5aee0dd8aa52bdcd2391afd01fa8ab625edf333a
tugcekoksal <tugcekoksal35@gmail.com>
2023-11-09 14:47:35 +0100

    change

--- pages/index.js
+++ pages/index.js
@@ -11,22 +11,21 @@ export default function Home() {
       <div className="flex justify-center items-center">
         <p className=" ">Avukat Ebru KARACA</p>
       </div>
+      <div className="flex justify-center items-center"> </div>
 
-      <div className="relative mt-[10px] z-[9999]  flex w-[60%] place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full  before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <h1 className="text-3xl leading-loose ">
-          Sitemiz yapım aşamasındadır. Tüm sorularınız için{" "}
-          <span className=" underline ">
-            <a
-              href={"mailto:avebrukoksal@gmail.com"}
-              target="_blank"
-              className="text-red-400"
-            >
-              avebrukoksal@gmail
-            </a>
-          </span>{" "}
-          adresinden bize ulaşabilirsiniz.
-        </h1>
-      </div>
+      <h1 className=" text-xl sm:text-3xl leading-loose ">
+        Sitemiz yapım aşamasındadır. Tüm sorularınız için{" "}
+        <span className=" underline ">
+          <a
+            href={"mailto:avebrukoksal@gmail.com"}
+            target="_blank"
+            className="text-red-400"
+          >
+            avebrukoksal@gmail
+          </a>
+        </span>{" "}
+        adresinden bize ulaşabilirsiniz.
+      </h1>
     </main>
   );
 }
