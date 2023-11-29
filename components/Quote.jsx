08b5b62beffd95656fb3c7f61c01e4d849b7b3f8
tugcekoksal <tugcekoksal35@gmail.com>
2023-11-29 21:38:37 +0100

    change

--- components/Quote.jsx
+++ components/Quote.jsx
@@ -3,12 +3,18 @@ import { Translation } from "../Translation";
 
 const Quote = () => {
   return (
-    <div className="bg-fixed bg-cover custom-bg h-[400px] flex items-center justify-center flex-col">
-      <div className="bg-white bg-opacity-20 shadow-lg z-[10] sm:bg-opacity-10 px-2 sm:px-[100px]">
-        <p className=" text-white rounded-md  italic text-3xl    py-8">
+    <div
+      className="relative bg-fixed bg-cover h-[400px] flex items-center justify-center"
+      style={{
+        backgroundImage: 'url("/quote.jpg")',
+      }}
+    >
+      <div className="absolute inset-0 bg-black opacity-50"></div>
+      <div className="z-[10] bg-white bg-opacity-20 shadow-lg sm:bg-opacity-10 p-4 sm:px-[100px]">
+        <p className="text-white rounded-md italic text-3xl text-center py-4 sm:py-8">
           <Translation textKey="soz" />
         </p>
-        <p className=" text-white rounded-md italic text-xl text-center   py-4 ">
+        <p className="text-white rounded-md italic text-xl text-center py-2 sm:py-4">
           - J.F.Kennedy
         </p>
       </div>
