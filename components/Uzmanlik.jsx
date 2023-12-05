09d7279c61ea390495db6109803348b324962395
tugcekoksal <tugcekoksal35@gmail.com>
2023-12-05 16:52:07 +0100

    change

--- components/Uzmanlik.jsx
+++ components/Uzmanlik.jsx
@@ -17,7 +17,7 @@ const Uzmanlik = ({ numberOfCards, col }) => {
   return (
     <section id="uzmanlik" className=" ">
       <div
-        className={`grid grid-cols-1  lg:grid-cols-${col} gap-10 mx-6 md:mx-[7rem] my-[100px] h-full`}
+        className={`grid grid-cols-1  md:grid-cols-3 gap-10 mx-6 md:mx-[7rem] my-[100px] h-full`}
       >
         {/* {uzmanlik.slice(0, numberOfCards).map((uzmanlik, index) => (
           <UzmanlikKart
